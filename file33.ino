6c60ed3082d051efa6227eddc6ba5ea0401801ac
fatemeh <fatemehkerachiyan@gmail.com>
2025-12-04 22:51:01 +0330

    Add files via upload

---
+++ file33.ino
@@ -0,0 +1,20 @@
+const int SW = 2;
+const int X = 0;
+const int Y = 1;
+void setup() {
+  pinMode(SW, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("Switch:  ");
+  Serial.print(digitalRead(SW));
+  Serial.print("\t\t");
+  Serial.print("VRx:");
+  Serial.print(analogRead(X));
+  Serial.print("\t\t");
+  Serial.print("VRy: ");
+  Serial.println(analogRead(Y));
+  Serial.println("____");
+  delay(500);
+}
